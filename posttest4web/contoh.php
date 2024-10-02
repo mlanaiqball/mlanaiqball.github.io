5777c539ff34da69c456c29c756bfa0328d68975
mlanaiqball <163087067+mlanaiqball@users.noreply.github.com>
2024-10-02 23:30:26 +0800

    Add files via upload

---
+++ posttest4web/contoh.php
@@ -0,0 +1,29 @@
+<?php
+$nama = "daky";
+$umur = 26;
+$kelas = 23;
+if ($umur >= 12) {
+    #code...
+    echo 'umur =>12. umur saat ini', $umur; 
+} else {
+    echo'umur kurang dari 12';
+}
+switch ($nama) {
+    case 'daky' :
+        # code...
+        echo $nama;
+        break;
+
+    case 'iqbal':
+            #code...
+            echo $umur;
+            break;
+
+    default:
+    #code...
+    echo 'tidak ada';
+    break;    
+}
+
+$murid = array('nama'=)
+?>
